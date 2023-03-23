76133b5909f19951b78f1d2ca0bb653fab375fdb
Weblymit <weblymit@gmail.com>
2023-03-23 21:15:18 +0100

    crud => create

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
+use DateTime;
+use App\Entity\Post;
 use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
-    #[Route('/', name: 'app_blog', methods: ['GET'])]
+    // private $em;
+
+    // public function __construct(EntityManagerInterface $em)
+    // {
+    //     $this->em = $em;
+    // }
+    #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(PostRepository $repo): Response
     {
         // dd($repo->findAll());
@@ -34,8 +47,57 @@ class BlogController extends AbstractController
         $em->flush();
 
 
-        return $this->redirectToRoute('app_blog');
+        return $this->redirectToRoute('app_home');
         // return $this->render('blog/show.html.twig', compact('post'));
+    }
+
+    #[Route('/create', name: 'app_create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $post = new Post;
+        $form = $this->createFormBuilder($post)
+            // ->add('title', TextType::class, [
+            //     'attr' => ['class' => 'bg-blue-500'],
+            // ])
+            ->add('title', null, [
+                'attr' => ['class' => 'bg-blue-500'],
+            ])
+            ->add('category', null)
+            ->add('author', TextType::class)
+            // ->add('urlImg', TextType::class)
+            ->add('content', TextareaType::class)
+            // ->add('createdAt', DateType::class)
+            // ->add('save', SubmitType::class, ['label' => 'Enregistrer'])
+            ->getForm();
+
+        // handleRequest => recupere les data du formulaire
+        $form->handleRequest($request);
+
+        // verifie si form a été soumis et que les data sont formulaire
+        if ($form->isSubmitted() && $form->isValid()) {
+            // recupere data et on stoque
+            // $data = $form->getData();
+            // // dd($data);
+            // // crée new objet de l'entity
+            // $post = new Post;
+
+            // $post->setTitle($data['title']);
+            // $post->setCategory($data['category']);
+            // $post->setAuthor($data['author']);
+            // $post->setContent($data['content']);
+            $post->setUrlImg('https://media.gqmagazine.fr/photos/6418337591d17f9554133fe2/16:9/w_2560%2Cc_limit/IphoneApple.png');
+            $post->setCreatedAt(new DateTime());
+
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+
+        return $this->render('blog/create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     #[Route('/about', name: 'app_about', methods: ['GET'])]
--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -12,32 +12,57 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_post')]
-    public function index(): Response
-    {
-        return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
-        ]);
-    }
+    // #[Route('/post', name: 'app_post')]
+    // public function index(): Response
+    // {
+    //     return $this->render('post/index.html.twig', [
+    //         'controller_name' => 'PostController',
+    //     ]);
+    // }
 
 
-    #[Route('/post/create', name: 'create_post')]
-    public function createPost(EntityManagerInterface $entityManager): Response
-    {
-        $post = new Post();
-        $post->setTitle('Toto en Russie');
-        $post->setCategory('Politique');
-        $post->setAuthor('Toto');
-        $post->setContent("Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit est perferendis voluptatem fuga optio distinctio tempora quibusdam praesentium aspernatur ea excepturi, accusamus reiciendis similique molestiae id nisi illo maxime voluptatibus?");
-        $post->setUrlImg('https://media.ldlc.com/r1600/ld/products/00/05/82/93/LD0005829395_1.jpg');
-        $post->setCreatedAt(new DateTime());
+    // #[Route('/post/create', name: 'create_post')]
+    // public function createPost(EntityManagerInterface $entityManager): Response
+    // {
+    //     $post = new Post();
+    //     $post->setTitle('Toto en Russie');
+    //     $post->setCategory('Politique');
+    //     $post->setAuthor('Toto');
+    //     $post->setContent("Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit est perferendis voluptatem fuga optio distinctio tempora quibusdam praesentium aspernatur ea excepturi, accusamus reiciendis similique molestiae id nisi illo maxime voluptatibus?");
+    //     $post->setUrlImg('https://media.ldlc.com/r1600/ld/products/00/05/82/93/LD0005829395_1.jpg');
+    //     $post->setCreatedAt(new DateTime());
 
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($post);
+    //     // tell Doctrine you want to (eventually) save the Product (no queries yet)
+    //     $entityManager->persist($post);
 
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
+    //     // actually executes the queries (i.e. the INSERT query)
+    //     $entityManager->flush();
 
-        return new Response('Saved new product with id ' . $post->getId());
-    }
+    //     return new Response('Saved new product with id ' . $post->getId());
+    // }
+
+    // #[Route('/create', name: 'app_create', methods: ['GET', 'POST'])]
+    // public function create(Request $request, EntityManagerInterface $em)
+    // {
+    //     if ($request->isMethod('POST')) {
+    //         // $request->query() //recuperer data de type GET
+    //         $data = $request->request->all();
+    //         $post = new Post;
+    //         $post->setTitle('Toto en Russie');
+    //         $post->setCategory('Politique');
+    //         $post->setAuthor('Toto');
+    //         $post->setContent("Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit est perferendis voluptatem fuga optio distinctio tempora quibusdam praesentium aspernatur ea excepturi, accusamus reiciendis similique molestiae id nisi illo maxime voluptatibus?");
+    //         $post->setUrlImg('https://media.ldlc.com/r1600/ld/products/00/05/82/93/LD0005829395_1.jpg');
+    //         $post->setCreatedAt(new DateTime());
+
+    //         // prepare requete
+    //         $em->persist($post);
+    //         $em->flush();
+
+    //         // redirect
+    //         // return $this->redirect('/');
+    //         return $this->redirectToRoute('app_home');
+    //     }
+    //     return $this->render('blog/create.html.twig');
+    // }
 }
